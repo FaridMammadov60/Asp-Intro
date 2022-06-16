60a87d9a49c3f7377849e2d4b2a81ff3e38045a1
FaridMammadov60 <faridmma@code.edu.az>
2022-06-16 02:54:33 -0700

    add task

---
+++ FirstCode/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            //return Content($"isledi {id}");
+            return View();
+        }
+        public IActionResult About()
+        {
+            //return Content("about isledi");
+            return View();
+        }
+    }
+}
